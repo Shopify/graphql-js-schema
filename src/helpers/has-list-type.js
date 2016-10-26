795696063f38730f3fd8d408e1114d1c14c87a14
James MacAulay <james@shopify.com>
2016-10-26 13:35:21 -0400

    remove unneeded files

--- src/helpers/has-list-type.js
+++
@@ -1,9 +0,0 @@
-export default function hasListType(typeDescriptor) {
-  if (typeDescriptor.kind === 'LIST') {
-    return true;
-  } else if (typeDescriptor.ofType) {
-    return hasListType(typeDescriptor.ofType);
-  }
-
-  return false;
-}
