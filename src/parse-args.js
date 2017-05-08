61ae5821b6a0b2828b83132df31f7e3a2ccf4564
Mina Smart <mina.smart@shopify.com>
2017-05-08 11:07:01 -0400

    Capture the whitelist-config option

--- src/parse-args.js
+++ src/parse-args.js
@@ -1,5 +1,10 @@
 import minimist from 'minimist';
 
+function shouldShowHelp(args) {
+  return (args.help || !(args['schema-file'] && args.outdir)) ||
+    (args['whitelist-type'] && args['whitelist-config']);
+}
+
 export default function parseArgs(rawArgs) {
   const args = minimist(rawArgs, {
     boolean: 'bundle-only',
@@ -15,7 +20,7 @@ export default function parseArgs(rawArgs) {
     }
   });
 
-  if (args.help || !(args['schema-file'] && args.outdir)) {
+  if (shouldShowHelp(args)) {
     return {showHelp: true};
   }
 
@@ -24,6 +29,7 @@ export default function parseArgs(rawArgs) {
     outdir: args.outdir,
     schemaBundleName: args['schema-bundle-name'],
     bundleOnly: args['bundle-only'],
-    whitelistTypes: args['whitelist-type'] || false
+    whitelistTypes: args['whitelist-type'] || false,
+    whitelistConfig: args['whitelist-config']
   };
 }
--- test/parse-args-test.js
+++ test/parse-args-test.js
@@ -44,4 +44,21 @@ suite('parse-args-test', () => {
 
     assert.deepEqual(opts.whitelistTypes, ['Product', 'Collection', 'String', 'Money']);
   });
+
+  test('it parses a whitelist config json file', () => {
+    const opts = parseArgs([
+      '--whitelist-config', 'profile.json'
+    ].concat(requiredArgs));
+
+    assert.equal(opts.whitelistConfig, 'profile.json');
+  });
+
+  test('it shows help if you try to supply a whitelist type and a whitelist config', () => {
+    const opts = parseArgs([
+      '--whitelist-type', 'Product',
+      '--whitelist-config', 'profile.json'
+    ].concat(requiredArgs));
+
+    assert.equal(opts.showHelp, true);
+  });
 });
